7721ba0b9e6f83f99b529f3a993179add17d2dcf
trindadedev13 <devsuay@gmail.com>
2024-10-08 16:49:15 -0300

    feat: Enable EdgeToEdge

--- app/src/main/kotlin/dev/trindadedev/composeware/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/dev/trindadedev/composeware/ui/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.*
 import androidx.compose.material3.*
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ComposeWareTheme {
                 Surface(
